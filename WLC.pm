b734a3456d3e711face71abca90ce279fcbf3f39
Matthew Newton <mcn4@leicester.ac.uk>
2012-09-03 20:59:13 +0100

    initial checkin

---
+++ WLC.pm
@@ -0,0 +1,305 @@
+#! /usr/bin/perl -w
+
+package WLC;
+
+use Data::Dumper;
+use Net::SNMP;
+
+use WLC::AP;
+
+use strict;
+use vars qw($VERSION @ISA @EXPORT);
+use Carp;
+use Exporter;
+
+@ISA = qw(Exporter);
+@EXPORT = qw();
+$VERSION = '0.01';
+
+my %wlcs = ();
+
+
+sub new
+{
+  my ($class, %data) = @_;
+
+  my $self = {
+    ap_list     => undef,
+    wlc_list    => {},
+    cache_file  => undef,
+    cache_data  => undef,
+  };
+
+  bless $self, $class;
+  $self->init();
+
+  return $self;
+}
+
+sub init
+{
+  my ($self) = @_;
+
+}
+
+################################################################################
+# WLC functions
+
+#-------------------------------------------------------------------------------
+# add_wlc
+
+sub add_wlc
+{
+  my ($self, $ip, $data) = @_;
+
+  unless (defined $ip) {
+    carp "No IP address specificed";
+    return undef;
+  }
+
+  if (defined $$self{wlc_list}{$ip}) {
+    carp "WLC $ip already added";
+    return undef;
+  }
+
+  my $wlc = {
+    ip => $ip,
+    snmp_community => "public",
+    snmp_version => 1,
+    hostname => undef,
+  };
+
+  $$wlc{snmp_community} = delete $$data{community} if $$data{community};
+  $$wlc{snmp_version} = delete $$data{version} if $$data{version};
+
+  $self->{wlc_list}{$ip} = $wlc;
+
+  carp "add_wlc: unknown arguments" if scalar %$data;
+  
+  1;
+}
+
+#-------------------------------------------------------------------------------
+# _get_snmp_session
+
+sub _get_snmp_session
+{
+  my ($self, $wlc_ip) = @_;
+
+  return undef unless defined $self->{wlc_list}{$wlc_ip};
+
+  my $wlc = $self->{wlc_list}{$wlc_ip};
+
+  return $$wlc{snmp_session} if defined $$wlc{snmp_session};
+
+  my ($session, $error) = Net::SNMP->session(
+                            -version => $$wlc{snmp_version},
+                            -hostname => $wlc_ip,
+                            -community => $$wlc{snmp_community});
+
+  if (!defined $session) {
+    carp "error getting snmp session to $wlc_ip ($error)";
+    return undef;
+  }
+
+  $$wlc{snmp_session} = $session;
+
+  return $session;
+}
+
+
+################################################################################
+# AP functions
+
+#-------------------------------------------------------------------------------
+# get_aps
+
+sub get_aps
+{
+  my ($self) = @_;
+
+  $self->_query_wlcs_aps() unless defined $self->{ap_list};
+
+  return values %{$self->{ap_list}};
+}
+
+
+sub get_ap_by_name
+{
+  my ($self, $name) = @_;
+
+  $name = lc $name;
+
+  $self->_query_wlcs_aps() unless defined $self->{ap_list};
+
+  foreach my $ap (values %{$self->{ap_list}}) {
+    if (lc $ap->{name} eq $name) {
+      return $ap;
+    }
+  }
+
+  return undef;
+}
+
+
+sub get_ap_by_mac
+{
+  my ($self, $mac) = @_;
+
+  $mac = lc $mac;
+
+  $self->_query_wlcs_aps() unless defined $self->{ap_list};
+
+  foreach my $ap (values %{$self->{ap_list}}) {
+    if (lc $ap->{mac} eq $mac) {
+      return $ap;
+    }
+  }
+
+  return undef;
+}
+
+
+#-------------------------------------------------------------------------------
+# query_aps
+
+sub _query_wlcs_aps
+{
+  my ($self) = @_;
+  my $snmp_session;
+
+  my $baseoid = ".1.3.6.1.4.1.14179.2.2.1.1.3";
+  foreach my $wlc_ip (keys %{$self->{wlc_list}}) {
+    $snmp_session = $self->_get_snmp_session($wlc_ip);
+    $self->_get_generic_snmp_table($snmp_session, $baseoid,
+      sub {
+        my ($oid, $value) = @_;
+        my $mac = _get_mac_from_oid($oid);
+        $self->_add_update_ap($mac, $wlc_ip, {name => $value});
+      });
+  }
+}
+
+
+#-------------------------------------------------------------------------------
+# add or update values for an AP
+
+sub _add_update_ap
+{
+  my ($self, $ap_mac, $wlc_ip, $data) = @_;
+
+  unless (defined $self->{ap_list}{$ap_mac}) {
+    $self->{ap_list}{$ap_mac} = WLC::AP->new($self, $ap_mac, $wlc_ip);
+  }
+
+  foreach my $i (keys %$data) {
+    $self->{ap_list}{$ap_mac}->update($i, $$data{$i});
+  }
+
+#  return $self->{ap_list}{$ap_mac};
+}
+
+
+################################################################################
+# SNMP functions
+
+#-------------------------------------------------------------------------------
+# Generic read SNMP table
+
+sub _get_generic_snmp_table
+{
+  my ($self, $session, $tablebase, $callback) = @_;
+  my $count = 0;
+  my $oid;
+
+  my @args = (-varbindlist => [$tablebase]);
+
+  while (defined($session->get_next_request(@args))) {
+    $oid = ($session->var_bind_names())[0];
+
+    last unless Net::SNMP::oid_base_match($tablebase, $oid);
+
+    my $value = $session->var_bind_list()->{$oid};
+    &$callback($oid, $value);
+    $count++;
+
+    @args = (-varbindlist => [$oid]);
+  }
+
+  return $count;
+}
+
+
+#-------------------------------------------------------------------------------
+# Generic SNMP write to an AP
+
+sub _snmp_write_ap
+{
+  my ($self, $ap, $vbl) = @_;
+
+  my $wlc_ip = $ap->{wlc_ip};
+  my $snmp_session = $self->_get_snmp_session($wlc_ip);
+
+  my $r = $snmp_session->set_request(-varbindlist => $vbl);
+  return $r if defined $r;
+
+  # AP may have moved to a different controller
+  $self->_query_wlcs_aps();
+  my $wlc_ip2 = $ap->{wlc_ip};
+  return undef if $wlc_ip eq $wlc_ip2;
+
+  $snmp_session = $self->_get_snmp_session($wlc_ip2);
+  return $snmp_session->set_request(-varbindlist => $vbl);
+}
+
+#-------------------------------------------------------------------------------
+# Generic SNMP write
+
+sub _snmp_get_ap
+{
+  my ($self, $ap, $vbl) = @_;
+
+  my $wlc_ip = $ap->{wlc_ip};
+  my $snmp_session = $self->_get_snmp_session($wlc_ip);
+
+  my $r = $snmp_session->get_request(-varbindlist => $vbl);
+  return $r if defined $r;
+
+  # AP may have moved to a different controller
+  $self->_query_wlcs_aps();
+  my $wlc_ip2 = $ap->{wlc_ip};
+  return undef if $wlc_ip eq $wlc_ip2;
+
+  $snmp_session = $self->_get_snmp_session($wlc_ip2);
+  return $snmp_session->get_request(-varbindlist => $vbl);
+}
+
+
+#-------------------------------------------------------------------------------
+# Get MAC address from OID (last six octets)
+
+sub _get_mac_from_oid
+{
+  my ($oid, $base) = @_;
+  my $octets;
+
+  if (defined $base) {
+    unless ($oid =~ s/^$base.(\d+(?:\.\d+){5})$//) {
+      carp "cannot extract mac from oid ($oid) with base ($base)";
+      return undef;
+    }
+    $octets = $1;
+  } else {
+    unless ($oid =~ /(\d+(?:\.\d+){5})$/) {
+      carp "cannot extract mac from oid ($oid)";
+      return undef;
+    }
+    $octets = $1;
+  }
+
+  return lc join("", unpack("(H2)6", pack("C6", split(/\./, $octets))));
+}
+
+
+1;
+
---
+++ WLC/AP.pm
@@ -0,0 +1,212 @@
+#! /usr/bin/perl -w
+
+package WLC::AP;
+
+use Data::Dumper;
+
+use strict;
+use Net::SNMP;
+use vars qw($VERSION @ISA @EXPORT);
+use Carp;
+use Exporter;
+
+@ISA = qw(Exporter);
+@EXPORT = qw();
+$VERSION = '0.01';
+
+
+sub new
+{
+  my ($class, $wlc, $mac, $wlc_ip) = @_;
+
+  croak "invalid mac address format ($mac)" unless $mac =~ /^[a-f0-9]{12}$/i;
+
+  my $self = {
+    wlc         => $wlc,
+    mac         => $mac,
+    oidindex    => join(".", map {hex $_} unpack("(A2)6", $mac)),
+    wlc_ip      => $wlc_ip,
+    name        => undef,
+  };
+
+  bless $self, $class;
+  $self->init();
+
+  return $self;
+}
+
+sub init
+{
+  my ($self) = @_;
+
+}
+
+sub update
+{
+  my ($self, $key, $value) = @_;
+
+  $self->{$key} = $value;
+}
+
+#sub name
+#{
+#  my ($self, $new) = @_;
+#
+#  return $self->{name} unless defined $new;
+#
+#  my $oid = ".1.3.6.1.4.1.14179.2.2.1.1.3." . $self->{oidindex};
+#  my $r = $self->{wlc}->_snmp_write_ap($self, [$oid, OCTET_STRING, $new]);
+#
+#  if (defined $r) {
+#    if ($$r[0] eq $oid) {
+#      $self->{name} = $$r[1];
+#      return 1;
+#    } else {
+#      carp "unknown result from snmp set request=$oid response=$$r[0] ($$r[1])";
+#    }
+#  }
+#
+#  return 0;
+#}
+
+sub mac
+{
+  my ($self, $sep) = @_;
+
+  if (defined $sep) {
+    if ($sep eq ":" || $sep eq "-") {
+      return lc join($sep, unpack("(a2)6", $self->{mac}));
+    }
+    if ($sep eq ".") {
+      return lc join($sep, unpack("(a4)3", $self->{mac}));
+    }
+    croak "unknown separator '$sep'";
+  }
+
+  return $self->{mac};
+}
+
+sub wlcip
+{
+  my ($self) = @_;
+
+  return $self->{wlc_ip};
+}
+
+sub _oidindex
+{
+  my ($self) = @_;
+
+  return $self->{oidindex};
+}
+
+sub _generic_method
+{
+  my ($self, $name, $oidprefix, $new, $rw, $type) = @_;
+  my $oid = $oidprefix . $self->{oidindex};
+  my $r;
+
+  unless (defined($new)) {
+    return $self->{$name} if defined $self->{$name};
+
+    $r = $self->{wlc}->_snmp_get_ap($self, [$oid]);
+    return undef unless defined $r;
+    $self->{$name} = $$r{$oid} || undef;
+    return $self->{$name};
+  }
+
+  if (defined $rw and !$rw) {
+    carp "attempt to set read only value";
+    return undef;
+  }
+
+  $r = $self->{wlc}->_snmp_write_ap($self, [$oid, $type, $new]);
+
+  if (defined $r) {
+    if (defined $$r{$oid}) {
+      $self->{$name} = $$r{$oid};
+      return $self->{$name};
+    } else {
+      carp "unknown result from snmp set request=$oid response=$$r[0] ($$r[1])";
+    }
+  }
+
+  return undef;
+}
+
+#sub location
+#{
+#  my ($self, $new) = @_;
+#
+#  return $self->_generic_method("location", ".1.3.6.1.4.1.14179.2.2.1.1.4.", $new, 1);
+#}
+
+my %_methods = (
+    "name"            => [ ".1.3.6.1.4.1.14179.2.2.1.1.3.", 1, OCTET_STRING],
+    "location"        => [ ".1.3.6.1.4.1.14179.2.2.1.1.4.", 1, OCTET_STRING],
+    "operationstatus" => [ ".1.3.6.1.4.1.14179.2.2.1.1.6.", 0, INTEGER],
+    "softwareversion" => [ ".1.3.6.1.4.1.14179.2.2.1.1.8.", 0, OCTET_STRING],
+    "bootversion"     => [ ".1.3.6.1.4.1.14179.2.2.1.1.9.", 0, OCTET_STRING],
+    "primarywlc"      => [ ".1.3.6.1.4.1.14179.2.2.1.1.10.", 1, OCTET_STRING],
+    "resetap"         => [ ".1.3.6.1.4.1.14179.2.2.1.1.11.", 1, INTEGER],
+    "model"           => [ ".1.3.6.1.4.1.14179.2.2.1.1.16.", 0, OCTET_STRING],
+    "serialnumber"    => [ ".1.3.6.1.4.1.14179.2.2.1.1.17.", 0, OCTET_STRING],
+    "clearconfig"     => [ ".1.3.6.1.4.1.14179.2.2.1.1.19.", 1, INTEGER],
+    "ipaddress"       => [ ".1.3.6.1.4.1.14179.2.2.1.1.19.", 1, IPADDRESS],
+    "secondarywlc"    => [ ".1.3.6.1.4.1.14179.2.2.1.1.23.", 1, OCTET_STRING],
+    "tertiarywlc"     => [ ".1.3.6.1.4.1.14179.2.2.1.1.24.", 1, OCTET_STRING],
+    "isstaticip"      => [ ".1.3.6.1.4.1.14179.2.2.1.1.25.", 1, INTEGER],
+    "netmask"         => [ ".1.3.6.1.4.1.14179.2.2.1.1.26.", 1, IPADDRESS],
+    "gateway"         => [ ".1.3.6.1.4.1.14179.2.2.1.1.27.", 1, IPADDRESS],
+    "staticipaddress" => [ ".1.3.6.1.4.1.14179.2.2.1.1.28.", 1, IPADDRESS],
+    "apgroup"         => [ ".1.3.6.1.4.1.14179.2.2.1.1.30.", 1, OCTET_STRING],
+    "ethernetmac"     => [ ".1.3.6.1.4.1.14179.2.2.1.1.33.", 0, OCTET_STRING],
+    "adminstatus"     => [ ".1.3.6.1.4.1.14179.2.2.1.1.37.", 1, INTEGER],
+  );
+
+# INTEGER, INTEGER32, OCTET_STRING, OBJECT_IDENTIFIER, IPADDRESS, COUNTER, COUNTER32, GAUGE,
+# GAUGE32, UNSIGNED32, TIMETICKS, OPAQUE, COUNTER64
+
+
+{
+  foreach my $func (keys %_methods) {
+    eval "sub $func { return \$_[0]->_generic_method(\"$func\", \"" .
+          $_methods{$func}[0] . "\", \$_[1], " . $_methods{$func}[1] . ", " .
+          $_methods{$func}[2] . ");}"
+  }
+}
+
+
+
+
+#sub location
+#{
+#  my ($self, $new) = @_;
+#  my $oid = ".1.3.6.1.4.1.14179.2.2.1.1.4." . $self->{oidindex};
+#  my $r;
+#
+#  unless (defined($new)) {
+#    return $self->{location} if defined $self->{location};
+#
+#    $r = $self->{wlc}->_snmp_get_ap($self, [$oid]);
+#    return undef unless defined $r;
+#    $self->{location} = $$r{$oid} || undef;
+#    return $self->{location};
+#  }
+#
+#  $r = $self->{wlc}->_snmp_write_ap($self, [$oid, OCTET_STRING, $new]);
+#
+#  if (defined $r) {
+#    if (defined $$r{$oid}) {
+#      $self->{location} = $$r{$oid};
+#      return $self->{location};
+#    } else {
+#      carp "unknown result from snmp set request=$oid response=$$r[0] ($$r[1])";
+#    }
+#  }
+#
+#  return undef;
+#}
+
+1;
+
